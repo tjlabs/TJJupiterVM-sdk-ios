2423fda2a8a0a151c5d43a3bd53142d2d87296eb
tjlabs-dev <92098727+tjlabs-dev@users.noreply.github.com>
2026-09-15 22:48:21 +0300

    update 1.0.20
    1.0.20

--- TJJupiterVMSDK/Classes/TJJupiterVMAuth.swift
+++ TJJupiterVMSDK/Classes/TJJupiterVMAuth.swift
@@ -30,9 +30,9 @@ public class TJJupiterVMAuth {
     private func makeClientMeta() -> ClientMeta {
         let clientSdks = [
             SdkMeta(name: "TJLabsAuth", version: "1.0.7"),
-            SdkMeta(name: "TJLabsCommon", version: "1.0.8"),
+            SdkMeta(name: "TJLabsCommon", version: "1.0.9"),
             SdkMeta(name: "TJLabsResource", version: "0.1.15"),
-            SdkMeta(name: "TJLabsJupiter", version: "2.0.17"),
+            SdkMeta(name: "TJLabsJupiter", version: "2.0.18"),
             SdkMeta(name: "TJLabsJupiterVM", version: "2.0.17")
         ]
         
